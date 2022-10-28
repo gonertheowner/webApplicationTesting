9467775ae7bf2eed2e9fcc53ea63897c4a5517e2
Kirill Babich <gonertheowner@gmail.com>
2022-10-28 21:55:33 +0300

    Filled SearchPage.java

--- webApplicationTesting/src/test/java/babich/projects/SearchPage.java
+++ webApplicationTesting/src/test/java/babich/projects/SearchPage.java
@@ -1,4 +1,26 @@
 package babich.projects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
 public class SearchPage {
+    public WebDriver driver;
+    public SearchPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    @FindBy(xpath = "//input[@name='q']")
+    private WebElement searchField;
+
+    @FindBy(xpath = "//h2[@data-test-id='articleTitle']")
+    private List<WebElement> articles;
+
+    public void setSearchField(String searchField) {
+        this.searchField.sendKeys(searchField + "\n");
+    }
 }
